b6e33ca4a0b44d23f2b3b4e54641ac1cd366ebfb
sepfianda <sepfiandaekawidhira23@gmail.com>
2024-02-13 11:26:18 +0700

    day4 exercise

---
+++ day4-exercise.js
@@ -0,0 +1,46 @@
+//NO1
+//Create a function that can create a triangle pattern according to the height we provide like the 
+// following :
+// 01
+// 02 03
+// 04 05 06
+// 07 08 09 10
+// Parameters : height → triangle height
+
+
+
+//NO 2
+//Create a function that can loop the number of times according to the input we provide, and will 
+// replace multiples of 3 with "Fizz", multiples of 5 with "Buzz", multiples of 3 and 5 with 
+// "FizzBuzz".
+// ● Parameters : n → total looping
+// ○ Example: n = 6 →1, 2, Fizz, 4, Buzz, Fizz
+// ○ Example: n = 15 → 1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 12, 13, 14, FizzBuzz
+
+
+//NO3
+// Create a function to calculate Body Mass Index (BMI)
+// Formula : BMI = weight (kg) / (height (meter))²
+// Parameters : weight & height
+// Return values :
+// < 18.5 return “less weight”
+// 18.5 - 24.9 return “ideal”
+// 25.0 - 29.9 return “overweight”
+// 30.0 - 39.9 return “very overweight”
+// > 39.9 return “obesity”
+
+//NO4
+//Write a function to remove all odd numbers in an array and return a new array that contains even numbers only
+//Example : [1,2,3,4,5,6,7,8,9,10] → [2,4,6,8,10]
+const number4 = [1,2,3,4,5,6,7,8,9,10];
+const result4 = number4.filter((number) => )
+
+
+//NO5
+//Write a function to split a string and convert it into an array of words
+//Example : “Hello World” → [“Hello”, “World”]
+ const kata5 = "Hello World"
+ const array5 = kata5.split(" ")
+ console.log(array5);
+ 
+
